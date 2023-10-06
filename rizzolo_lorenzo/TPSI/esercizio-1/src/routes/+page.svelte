52a7da42caf85537b633218aacfb959c44851171
lorenzo-rizzolo <lorenzo.rizzolo@rizzoloimpianti.it>
2023-10-06 12:38:07 +0200

    home page svelte esercizio-1

--- rizzolo_lorenzo/TPSI/esercizio-1/src/routes/+page.svelte
+++ rizzolo_lorenzo/TPSI/esercizio-1/src/routes/+page.svelte
@@ -18,11 +18,11 @@
 			</picture>
 		</span>
 
-		to your new<br />SvelteKit app
+		Alla tua nuova<br>applicazione SvelteKit app
 	</h1>
 
 	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
+		prova a editare il file <strong>src/routes/+page.svelte</strong>
 	</h2>
 
 	<Counter />
