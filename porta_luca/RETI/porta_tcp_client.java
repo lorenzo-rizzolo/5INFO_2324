0bd6098db31a603000604393feed28f6f5d0ec48
Luca-Porta <luca.porta@istitutoagnelli.it>
2023-10-09 12:12:13 +0200

    Ultima modifica Esercitazione 8 e 9

--- porta_luca/RETI/porta_tcp_client.java
+++ porta_luca/RETI/porta_tcp_client.java
@@ -1,30 +1,41 @@
-import java.net.*;
 import java.io.*;
-import java.util.Date;
+import java.net.*;
 
 public class porta_tcp_client {
     public static void main(String[] args) {
         Socket client_socket;
-        
-        if (args.length != 3) {
-            System.out.println("uso: tcp_client  <IP> <porta> <string>");
+
+   
+        if (args.length != 2) {
+            System.out.println("uso: tcp_client <INDIRIZZO> <PORTA>");
             return;
         }
-        
-        String ip = args[0];
-        int tcp_port = Integer.parseInt(args[1]);
+        String address = args[0];
+        int port = Integer.parseInt(args[1]);
 
         try {
-            client_socket = new Socket(ip, tcp_port);
-            System.out.format("Socket connesso con il server %s sulla porta %d\n", ip, tcp_port);
+       
+            client_socket = new Socket(address, port);
+            System.out.println("Client TCP connesso a " + address + ":" + port);
+
+   
+            System.out.println("Inserisci un messaggio per inviarlo al server: ");
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            String messaggio = input.readLine();
 
             OutputStream output = client_socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
-            writer.println(args[2]);
+            writer.println(messaggio);
 
-            System.out.format("Inviato %d bytes con successo\n", args[2].length());
-            System.out.format("Risposta dal server %s\n", args[2]);
-            
+           
+            InputStream input_server = client_socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input_server));
+            String messaggio_server = reader.readLine();
+
+          
+            System.out.println("Messaggio ricevuto dal server: " + messaggio_server);
+
+          
             client_socket.close();
         }
         catch (IOException e) {
