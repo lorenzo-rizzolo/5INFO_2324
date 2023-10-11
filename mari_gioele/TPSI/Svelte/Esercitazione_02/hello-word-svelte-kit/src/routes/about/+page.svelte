523d79c107a2d075b25ad414a38ff39812f72016
gioele-mari <gioele.mari@istitutoagnelli.it>
2023-10-11 12:23:20 +0200

    mari_gioele

---
+++ mari_gioele/TPSI/Svelte/Esercitazione_02/hello-word-svelte-kit/src/routes/about/+page.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About Me!!!</h1>
+
+	<p>
+		Ciao io sono <a href="/sverdle">Antonio Mancuso</a>  e insegno Informatica ai ragazzi dell'Agnelli <br> Il mio blog si trova qui
+	</p>
+
+	<pre>https://profmancusoa.github.io/</pre>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+
+	<p>
+		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
+		using it with JavaScript disabled!
+	</p>
+</div>
