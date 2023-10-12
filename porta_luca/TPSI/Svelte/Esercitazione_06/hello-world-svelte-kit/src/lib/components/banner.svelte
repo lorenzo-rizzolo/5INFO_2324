4232639bf69f361f45ed0b23e3ed482d87cb5646
Luca-Porta <luca.porta@istitutoagnelli.it>
2023-10-12 16:08:08 +0200

    Esercitazione 06

---
+++ porta_luca/TPSI/Svelte/Esercitazione_06/hello-world-svelte-kit/src/lib/components/banner.svelte
@@ -0,0 +1,15 @@
+<script>
+    let nome='John Doe';
+</script>
+
+<section>
+	<div class="Bordo"><h1>Mi chiamo {nome}!</h1></div>
+</section>
+
+<style>
+	.Bordo{
+        color: orangered;
+        border: 10px solid lime;
+        border-radius: 20px;
+    }
+</style>
