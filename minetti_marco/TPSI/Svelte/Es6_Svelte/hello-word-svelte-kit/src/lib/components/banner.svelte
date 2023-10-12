e02362af46c58ec7a1b02ff982c018c9f1232984
Marco-Minetti <marco.minetti@istitutoagnelli.it>
2023-10-12 15:41:27 +0200

    add es6-7

---
+++ minetti_marco/TPSI/Svelte/Es6_Svelte/hello-word-svelte-kit/src/lib/components/banner.svelte
@@ -0,0 +1,17 @@
+<script>
+    let name = 'Jon doe'
+</script>
+
+<h1>Il mio nome è {name}!</h1>
+
+<style>
+    h1 {
+        width: 30%;
+        border: 10px solid blueviolet;
+        border-radius: 30px;
+        color: red;
+        text-align: center;
+        margin: 30px auto;
+        padding: 20px;
+    }
+</style>
---
+++ minetti_marco/TPSI/Svelte/Es7_Svelte /hello-word-svelte-kit/src/lib/components/banner.svelte
@@ -0,0 +1,17 @@
+<script>
+    let name = 'Maro Minetts'
+</script>
+
+<h1>Il mio nome è {name.toLocaleUpperCase()}!</h1>
+
+<style>
+    h1 {
+        width: 30%;
+        border: 10px solid blueviolet;
+        border-radius: 30px;
+        color: red;
+        text-align: center;
+        margin: 30px auto;
+        padding: 20px;
+    }
+</style>
