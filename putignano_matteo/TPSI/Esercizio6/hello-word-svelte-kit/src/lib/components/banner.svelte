a6321f620ba6d5a2a576348c91e14b575a46f8e1
Matteo-Putignano <matteo.putignano@istitutoagnelli.it>
2023-10-12 15:38:08 +0200

    aggiunta esercizio 6

---
+++ putignano_matteo/TPSI/Esercizio6/hello-word-svelte-kit/src/lib/components/banner.svelte
@@ -0,0 +1,13 @@
+<h1>My name is Puty!</h1>
+
+<style>
+	h1 {
+		width: 400px;
+		border: 20px solid green;
+		border-radius: 20px;
+		color: red;
+		text-align: center;
+		margin: 30px auto;
+		padding: 20px;
+	}
+</style>
