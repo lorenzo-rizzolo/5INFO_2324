839fbef1d5a691c80a790366d8182a667297722e
Matteo-Putignano <matteo.putignano@istitutoagnelli.it>
2023-10-12 15:10:55 +0200

    aggiunta esercizio 5

---
+++ putignano_matteo/TPSI/Esercizio5/hello-word-svelte-kit/src/routes/about/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About me</h1>
+
+	<p>
+		Ciao io sono Matteo Putignano e frequento la 5^ superiore presso l'istituto internazionale Edoardo Agnelli nell'indirizzo tecnico informatico.
+	</p>
+
+	<pre>Ma non faccio solo quello nella vita</pre>
+
+	<p>
+		Oltre all'informatica ho altre passioni come andare in palestra, uscire con gli amici e giocare con i miei amici online.
+	</p>
+
+	<p>
+		Frequento anche lezioni di scuola guida per prendere la patente B1 e faccio parte del gruppo scout CNGEI San Mauro 4 nella sezione di Gassino Torinese
+	</p>
+</div>
