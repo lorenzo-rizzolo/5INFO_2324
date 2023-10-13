9d148dba37ba4fca5c60733bc0505f2ea0a319c4
DUDU <davide.emanuel@istitutoagnelli.it>
2023-10-13 12:33:04 +0200

    aggiungo es 10 ma è nominato come es 9 poichè modificato

---
+++ emanuel_davide/TPSI/Svelte/ES9/src/routes/input/+page.svelte
@@ -0,0 +1,20 @@
+
+
+<script>
+    import input from "../../lib/components/input.svelte";
+    let nome,cognome,eta;
+</script>
+
+    
+
+
+<h1>input page</h1>
+
+Nome<input bind:value={nome}>
+Cognome<input bind:value={cognome}>
+Eta<input bind:value={eta}>
+
+<br><br>
+
+Io sono {nome} {cognome} e ho {eta} anni
+
