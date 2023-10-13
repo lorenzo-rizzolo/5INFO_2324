ce780705612a5944c26216facc85a8d621454d19
DUDU <davide.emanuel@istitutoagnelli.it>
2023-10-13 12:43:26 +0200

    consegno es 10 e 11 completi

---
+++ emanuel_davide/TPSI/Svelte/ES10-11/src/lib/components/WhoAmI.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let nome = "Davide";  
+    export let cognome = "Emanuel"; 
+    export let eta= "18";   
+    export let src;
+</script>
+<style>
+    
+
+</style>
+<section>
+    <h1>ciao sono {nome} {cognome} ed ho {eta} anni </h1>
+    <h4>studio presso l'isituto edoardo agenelli di Torino</h4>
+</section>
+<!-- svelte-ignore a11y-missing-attribute -->
+<img {src}>
+
