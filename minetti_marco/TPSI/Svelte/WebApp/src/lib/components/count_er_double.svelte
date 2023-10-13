fe5d6d658bef8e243763013db934f87af3621c6d
Marco-Minetti <marco.minetti@istitutoagnelli.it>
2023-10-13 13:46:44 +0200

    add es13

---
+++ minetti_marco/TPSI/Svelte/WebApp/src/lib/components/count_er_double.svelte
@@ -0,0 +1,81 @@
+<svelte:head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap"rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kablammo&display=swap" rel="stylesheet">
+</svelte:head>
+
+<script>
+    
+
+    function counter_incr () {
+        counter += 3;
+    }
+
+    function counter_decr () {
+        counter -= 3;
+    }
+
+    export let counter = 0;
+
+</script>
+
+<div class="container">
+    <div class="pulsante">
+        <button on:click={counter_decr}>
+            <span class="material-symbols-outlined icon">remove</span>
+        </button>
+    </div>
+    <div class="number" >
+        {counter}
+    </div>
+    <div class="pulsante">
+        <button on:click={counter_incr}>
+            <span class="material-symbols-outlined icon">add</span>
+        </button>
+    </div>
+</div>
+
+<style>
+    .container {
+        border: 4px solid blue;
+        display: flex;
+        justify-content: space-between;
+        border-radius: 30px;
+        width: 300px;
+        padding: 10px;
+    }
+
+    .pulsante {
+        display: flex;
+        align-items: center;
+    }   
+
+    button {
+        border: none;
+        background: none;
+    }
+
+    .icon {
+        font-size: 2.5rem;
+        color: violet;
+        font-weight: bolder;
+    }
+
+        .icon:hover {
+            color: blueviolet;
+            cursor: pointer;
+        }
+
+        .icon:active {
+            text-shadow: 1px 1px 10px blue;
+        }
+
+    .number {
+        font-size: 4rem;
+        font-family: 'Kablammo';
+        color: blueviolet;
+    }
+
+</style>
--- minetti_marco/TPSI/Svelte/WebApp/src/routes/counter/+page.svelte
+++ minetti_marco/TPSI/Svelte/WebApp/src/routes/counter/+page.svelte
@@ -1,10 +1,67 @@
 <script>
-    import Contatore from "../../lib/components/count_er.svelte" ;
+    import { createEventDispatcher } from "svelte";
+    import Contatore from "../../lib/components/count_er.svelte";
+    import Contatore_double from "../../lib/components/count_er_double.svelte";
     let contatore;
+    let contatore_double;
+    let numeri = [1, 2, 3, 4, 5];
+    let sottolineato = true;
+    let doppio;
+
+    $: doppio = contatore * 2;
 </script>
 
 <h1>Pagina Counter!!!</h1>
 
-<h2>Il valore del contenitore vale: {contatore}</h2>
+<h2 class={contatore < 5 ? 'rosso' : 'verde'}>Il valore del contenitore vale: {contatore}</h2>
+
+<h2>
+    Il suo doppio vale: {doppio}
+</h2>
+
+
+
+<!--     
+<h2 class:sottolineato>
+    Il valore del contatore vale: {contatore}
+</h2>
+
+{#if contatore < 5} 
+
+<h2 class="rosso">Il valore del contenitore vale: {contatore}</h2>
+
+{:else}
+
+<h2 class="verde">Il valore del contenitore vale: {contatore}</h2>
+
+{/if} -->
 
 <Contatore bind:counter={contatore}/>
+
+<br><br>
+
+<h2 class={contatore_double % 2 == 0 ? 'sottolineato' : ''}>Il valore del contenitore vale: {contatore_double}</h2>
+
+<Contatore_double bind:counter={contatore_double}/>
+
+<ul>
+    {#each numeri as numero}
+
+        <li>numeri[]: {numero}</li>
+
+    {/each}
+</ul>
+
+<style>
+    .rosso {
+        color: red;
+    }
+
+    .verde {
+        color: lightgreen;
+    }
+
+    .sottolineato {
+        text-decoration: underline;
+    }
+</style>
