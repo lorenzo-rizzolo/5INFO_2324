09c21a4e671096bb6fd4f1fddadf662a183e47d4
DUDU <davide.emanuel@istitutoagnelli.it>
2023-10-13 11:40:38 +0200

    consegno es 9 completo

--- emanuel_davide/TPSI/Svelte/ES9/src/lib/components/WhoAmI.svelte
+++ emanuel_davide/TPSI/Svelte/ES9/src/lib/components/WhoAmI.svelte
@@ -1,8 +1,3 @@
-<section>
-    <h1 class="main_page">pagina:chi sono?</h1>
-</section>
-
-
 <script>
     export let nome = "Davide";  
     export let cognome = "Emanuel"; 
@@ -10,18 +5,7 @@
     export let src;
 </script>
 <style>
-    .main_page{
-    color: #03045e;
-        padding: 50px;
-        font-size: 30px;
-        margin-bottom: 10px;
-        margin-left: 10px;
-        margin-top: 10px;
-        margin-right: 10px;
-        flex: 8;
-        text-align: center;
-        border: 2px solid green;
-}
+    
 
 </style>
 <section>
--- emanuel_davide/TPSI/Svelte/ES9/src/routes/chi_sono/+page.svelte
+++ emanuel_davide/TPSI/Svelte/ES9/src/routes/chi_sono/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 <section>
     <WhoAmI nome = "Davide" cognome = "Emanuel" eta = "18" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0_E7RV9BmV2n6FK9t2nl3Q_D_B2NrOSYF3A&usqp=CAU"/>
-    <WhoAmI nome = "Davide" cognome = "Emanuel" eta = "18"/>
+    
 </section>
 
 
