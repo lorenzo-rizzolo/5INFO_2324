3907b3d0892b5642b3acfc42e5d36dc40a6d8238
DUDU <davide.emanuel@istitutoagnelli.it>
2023-10-13 10:49:42 +0200

    aggiungo es 9

---
+++ emanuel_davide/TPSI/Svelte/ES9/src/routes/chi_sono/+page.svelte
@@ -0,0 +1,15 @@
+<h1 class="main_page">pagina:chi sono?</h1>
+<style>
+    .main_page{
+    color: #03045e;
+        padding: 50px;
+        font-size: 30px;
+        margin-bottom: 10px;
+        margin-left: 10px;
+        margin-top: 10px;
+        margin-right: 10px;
+        flex: 8;
+        text-align: center;
+        border: 2px solid green;
+}
+</style>
