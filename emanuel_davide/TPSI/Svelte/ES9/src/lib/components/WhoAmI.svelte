3e177d558902fe0c8ddd25b898a9f464777ac03c
DUDU <davide.emanuel@istitutoagnelli.it>
2023-10-13 11:37:37 +0200

    modifiche es 9

---
+++ emanuel_davide/TPSI/Svelte/ES9/src/lib/components/WhoAmI.svelte
@@ -0,0 +1,33 @@
+<section>
+    <h1 class="main_page">pagina:chi sono?</h1>
+</section>
+
+
+<script>
+    export let nome = "Davide";  
+    export let cognome = "Emanuel"; 
+    export let eta= "18";   
+    export let src;
+</script>
+<style>
+    .main_page{
+    color: #03045e;
+        padding: 50px;
+        font-size: 30px;
+        margin-bottom: 10px;
+        margin-left: 10px;
+        margin-top: 10px;
+        margin-right: 10px;
+        flex: 8;
+        text-align: center;
+        border: 2px solid green;
+}
+
+</style>
+<section>
+    <h1>ciao sono {nome} {cognome} ed ho {eta} anni </h1>
+    <h4>studio presso l'isituto edoardo agenelli di Torino</h4>
+</section>
+<!-- svelte-ignore a11y-missing-attribute -->
+<img {src}>
+
--- emanuel_davide/TPSI/Svelte/ES9/src/routes/chi_sono/+page.svelte
+++ emanuel_davide/TPSI/Svelte/ES9/src/routes/chi_sono/+page.svelte
@@ -1,15 +1,13 @@
-<h1 class="main_page">pagina:chi sono?</h1>
-<style>
-    .main_page{
-    color: #03045e;
-        padding: 50px;
-        font-size: 30px;
-        margin-bottom: 10px;
-        margin-left: 10px;
-        margin-top: 10px;
-        margin-right: 10px;
-        flex: 8;
-        text-align: center;
-        border: 2px solid green;
-}
-</style>
+
+<script>
+    import WhoAmI from "../../lib/components/WhoAmI.svelte";
+    
+</script>
+<section>
+    <WhoAmI nome = "Davide" cognome = "Emanuel" eta = "18" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0_E7RV9BmV2n6FK9t2nl3Q_D_B2NrOSYF3A&usqp=CAU"/>
+    <WhoAmI nome = "Davide" cognome = "Emanuel" eta = "18"/>
+</section>
+
+
+
+
