8a0fd1e0c866d65f5520a3b3d153f32d5ec06742
SebastianoPeluso <sebastiano.peluso@istitutoagnelli.it>
2023-10-14 21:46:36 +0200

    add Esercitazione_09 e 10

---
+++ peluso.sebastiano/TPSI/svelte/Esercitazione_09/WebApp/src/lib/components/who_am_i.svelte
@@ -0,0 +1,25 @@
+
+<script>
+    export let nome = "Sebastiano";
+    export let cognome = "Peluso";
+    export let età = 18;
+    export let città = "Torino";
+    let larghezza = "300px";
+    let altezza = "300px";
+    import avatar from '$lib/images/avatar.jpg';
+    
+</script>
+
+<h1>Ciao, io sono {nome} {cognome} ed ho {età} anni</h1>
+<h4>Sono uno studente della 5 INFORMATICA dell'istituto internazionale Edoardo Agnelli di {città}</h4>
+
+<img class="foto" src={avatar}  alt="avatar" width="{larghezza}" height="{altezza}">
+
+
+
+ <style>
+    .foto{
+        border-radius: 80%;
+        
+    }
+ </style>
