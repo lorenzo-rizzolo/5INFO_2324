9ad8ae205e66f07542c140e7d99b7681370475e8
Alberto Amort <alberto.amort@istitutoagnelli.it>
2023-10-17 00:00:33 +0200

    es11

---
+++ amort_alberto/TPSI/SVELTE/Es11_Svelte/WebApp/src/routes/Counter/+page.svelte
@@ -0,0 +1,13 @@
+
+<script>
+    import Counter from "./counter.svelte";
+
+	let counter = 0; // stato di un contatore
+    function incremente() {
+        counter++;
+    }
+</script>
+
+<h1>Page Counter !!!</h1>
+
+<Counter/>
