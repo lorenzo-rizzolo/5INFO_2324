cb56251af5338ed42e633e1727548a824183afd9
Marco-Tranaso <marco.tranaso@istitutoagnelli.it>
2023-10-19 15:06:32 +0200

    es9,10,11

---
+++ tranaso_marco/TPSI/Svelte/es10/src/routes/input/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    let nome=" ";
+    let cognome=""; 
+    let eta="";
+    let confermato=false;
+</script>
+
+    <h1>Input page</h1>
+
+    Nome <input bind:value={nome}>
+    Cognome <input bind:value={cognome}>
+    Eta <input bind:value={eta}>
+    
+    <br><br>
+    confermato <input type="checkbox" bind:checked={confermato}>
+    <br><br>
+    Io sono {nome} {cognome} ed ho {eta} anni
+
+    <br><br>
+
+    <button disabled={!confermato}>Invia</button>
