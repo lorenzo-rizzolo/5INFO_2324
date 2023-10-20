77d8be5bbeee641b81c48f1dbc0aeddb599ac4ec
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2023-10-20 11:45:43 +0200

    add es12

---
+++ scattaglia_alessandro/TPSI/Svelte/es12/src/lib/components/who_am_i.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let nome = "Alessandro";
+    export let cognome = "Scattaglia";
+    export let età = 17;
+    let src =
+        "https://lh3.googleusercontent.com/qqbcSwJ2DOf7gFueSX16oLuzEIwqWpRHALA2ReXaEt1npxZ_LzZQtuW5ck4SUl-iODUuEtmpXszuQmLTx6Naceublg5iLv9OKy02";
+    export let larghezza = "30%";
+</script>
+
+<main>
+    <h1>
+        Ciao sono {nome}
+        {cognome}, ho {età} anni e frequento la 5 superiore alla scuola Istituto
+        tecnico Edoardo agnelli sezione informatica.
+    </h1>
+    <h4>
+        Pratico Atletica leggera da quando ho iniziato la terza superiore e mi
+        piace molto.
+    </h4>
+    <img class="img1" {src} width={larghezza} alt="Alessandro_Scattaglia" />
+</main>
+
+<style>
+    h1 {
+        font-size: x-large;
+    }
+    h4 {
+        font-size: large;
+    }
+    .img1 {
+        border-radius: 100%;
+    }
+</style>
---
+++ scattaglia_alessandro/TPSI/Svelte/es12/src/routes/Counter/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import DoubleCounter from "../../lib/components/double_counter.svelte";
+    import Counter from "../../lib/components/counter.svelte";
+    let contatore = 0;
+</script>
+
+<main>
+    <h1>Pagina counter!!!</h1>
+    <h2>Il valore del contatore vale: {contatore}</h2>
+    <DoubleCounter bind:contatore />
+</main>
+
+<style>
+    main {
+        width: 100%;
+        height: 100%;
+    }
+    h1 {
+        font-size: xx-large;
+    }
+    main {
+        width: 100%;
+        height: 100%;
+    }
+</style>
