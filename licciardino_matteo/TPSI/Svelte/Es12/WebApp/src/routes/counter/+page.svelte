f5f67c6678bf82272033f3d9b51f4f9f81893182
Licciardino-Matteo <matteo.licciardino@istitutoagnelli.it>
2023-10-20 11:45:48 +0200

    Fatto es 12 di Svelte

---
+++ licciardino_matteo/TPSI/Svelte/Es12/WebApp/src/routes/counter/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Counter from "../../lib/components/counter.svelte";
+    import DoubleCounter from "../../lib/components/double_counter.svelte";
+</script>
+
+<h1>Pagina Counter!</h1>
+
+<Counter/>
+<DoubleCounter/>
---
+++ licciardino_matteo/TPSI/Svelte/Es13/WebApp/src/routes/counter/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Counter from "../../lib/components/counter.svelte";
+    import DoubleCounter from "../../lib/components/double_counter.svelte";
+
+    let contatore = 0;
+    let contatore2 = 0;
+</script>
+
+<h1>Pagina Counter!</h1>
+
+<h2>Il valore del contatore vale: {contatore}</h2>
+
+<Counter bind:counter={contatore}/>
+
+<h2>Il valore del secondo contatore vale: {contatore2}</h2>
+
+<DoubleCounter bind:counter={contatore2}/>
