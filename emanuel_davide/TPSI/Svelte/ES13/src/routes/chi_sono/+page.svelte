bc43c0281dd43fd52fd33881514b195933f8e667
DUDU <davide.emanuel@istitutoagnelli.it>
2023-10-20 13:02:10 +0200

    aggiungo es 13

---
+++ emanuel_davide/TPSI/Svelte/ES13/src/routes/chi_sono/+page.svelte
@@ -0,0 +1,12 @@
+
+<script>
+    import WhoAmI from "../../lib/components/WhoAmI.svelte";
+</script>
+<section>
+    <WhoAmI nome = "Davide" cognome = "Emanuel" eta = "18" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0_E7RV9BmV2n6FK9t2nl3Q_D_B2NrOSYF3A&usqp=CAU"/>
+    
+</section>
+
+
+
+
