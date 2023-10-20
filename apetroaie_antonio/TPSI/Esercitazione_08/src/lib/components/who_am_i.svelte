fff482995064d824c7205df8112b83275677024b
Antonio Apetroaie <antonio.apetroaie@istitutoagnelli.it>
2023-10-20 12:15:36 +0200

    consegna esercizi

---
+++ apetroaie_antonio/TPSI/Esercitazione_08/src/lib/components/who_am_i.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let nome ="Mario";
+    export let cognome ="Rossi";
+    export let eta ="19";
+    export let url = "/avatar-apetroaie.png";
+
+    export let width_image
+</script>
+
+<h1> Ciao io sono {nome} {cognome} ed ho {eta} Anni </h1>
+
+<img src={url} width={width_image} alt ="antonio">
