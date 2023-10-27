7ba8408d1546b780cb709c05e5fcf4902046b39d
Luca-Porta <luca.porta@istitutoagnelli.it>
2023-10-27 22:41:57 +0200

    Aggiunta esercitazione 15

---
+++ porta_luca/TPSI/Svelte/Web-App/Esercitazione_15/WebApp/src/lib/components/counter.svelte
@@ -0,0 +1,150 @@
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?
+    family=Material+Symbols+Outlined:opsz, wght, FILL, GRAD@48,400,0,0" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Bungee+Spice&display=swap"
+    rel="stylesheet">
+</svelte:head>
+
+<script>
+    import {counter_store} from '../js/store';
+    let un=true, verde = true;
+    export let cont = 0;
+    let doppio, triplo;
+    $:doppio=cont*2;
+    $:triplo=cont*3;
+    function counter_incr()  {
+        cont += 1;
+        $counter_store=cont;
+    }
+    function counter_decr()  {
+        cont -= 1;
+        $counter_store=cont;
+        }
+    let numeri=[1,2,3,4,5,6,7,8,9];
+</script>
+
+<main>
+    <header>
+        <h1>Web App</h1>
+    </header>
+    <section>
+        <nav>
+        <ul>
+            <li class="giallo"><a href="../">Home</a></li>
+        </ul>
+        </nav>
+        <article>
+            <h1>Pagina Counter!!!</h1>
+            <h2 class:un>Il contatore ha come valore un mezzo di: {doppio}</h2>
+            <h2 class:un>Il contatore ha come valore un terzo di: {triplo}</h2>
+            <h1 class:verde={$counter_store%2==0}>Il contatore ha il valore: {$counter_store}</h1>
+            <div class="container">
+                <div class="pulsante">
+                    <button on:click={counter_decr}><span class="material-symbols-outlined icon">-</span></button>
+                </div>
+                <div class="number">
+                    {cont}
+                </div>
+                <div class="pulsante">
+                    <button on:click={counter_incr}><span class="material-symbols-outlined icon">+</span></button>
+                </div>
+            </div>
+            <br><br>
+            {#each numeri as numero, i}
+                <li class="verde">numeri[{i}]={numero}</li>
+            {/each}
+        </article>
+    </section>
+    <footer>
+    <h4>Copyright &copy; Antonio Mancuso</h4>
+    </footer>
+</main>
+
+<style>
+main {
+    display: flex;
+    flex-direction: column; 
+    height: 100vh; 
+    box-sizing: border-box;
+    color: white;    
+}
+header {
+    background-color: #023e8a; 
+    text-align: center;
+}
+section {
+    display: flex; flex-direction: row;
+    flex: 10;
+}
+nav {
+    flex: 1;
+    background-color: #0077b6;
+}
+
+nav ul {
+list-style: none; 
+}
+nav ul li { 
+    padding: 10px;
+}
+nav ul li a {
+color: white; 
+text-decoration: none; 
+font-size: 30px; 
+font-weight: bolder;
+}
+article { 
+    color: #03045e;
+     padding: 20px;
+      font-size: 30px;
+       flex: 8;
+}
+footer {
+background-color: #03045e; 
+text-align: center; 
+font-size: 20px;
+flex: 1;
+}
+
+.container{
+        border: 4px solid #D43C2F;
+        display: flex;
+        justify-content: space-between;
+        border-radius: 30px;
+        width: 300px;
+        padding: 10px;
+    }
+.pulsante{
+    display: flex;
+    align-items: center;
+}
+button{
+    border: none;
+    background: none;
+}
+.icon{
+    font-size: 2.5rem;
+    color: #F09C00;
+    font-weight: bolder;
+}
+.icon {
+font-size: 2.5rem;
+color: #F09C00; font-weight: bolder;
+}
+.icon:hover {
+color: #D43C2F;
+cursor: pointer;
+}
+.icon:active {
+text-shadow: 1px 1px 10px #D43C2F;
+}
+.number {
+font-size: 4rem;
+font-family: 'Bungee Spice', cursive;
+}
+.giallo{border: 5px solid yellow;}
+.verde{color: lime;}
+.un{
+    text-decoration: underline;
+}
+</style>
---
+++ porta_luca/TPSI/Svelte/Web-App/Esercitazione_15/WebApp/src/routes/about/+page.svelte
@@ -0,0 +1,83 @@
+<script>
+    import About from "../../lib/components/About.svelte";
+</script>
+
+<main>
+    <header>
+        <h1>Web App</h1>
+    </header>
+    <section>
+        <nav>
+        <ul>
+            <li class="giallo"><a href="../">Home</a></li>
+        </ul>
+        </nav>
+        <article>
+            <div class="blu">
+                <h1>Questa è la pagina About</h1>
+                <About />
+            </div>
+        </article>
+    </section>
+    <footer>
+    <h4>Copyright &copy; Antonio Mancuso</h4>
+    </footer>
+    </main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column; 
+        height: 100vh; 
+        box-sizing: border-box;
+        color: white;    
+    }
+    header {
+        background-color: #023e8a; 
+        text-align: center;
+    }
+    section {
+        display: flex; flex-direction: row;
+        flex: 10;
+    }
+    nav {
+        flex: 1;
+        background-color: #0077b6;
+    }
+
+.blu{
+    border: 5px solid blue;
+    width: 50%;
+    height: 600px;
+}
+
+.giallo{
+    border: 5px solid yellow;
+    width: 300px;
+}
+
+nav ul {
+list-style: none; 
+}
+nav ul li { 
+    padding: 10px;
+}
+nav ul li a {
+color: white; 
+text-decoration: none; 
+font-size: 30px; 
+font-weight: bolder;
+}
+article { 
+    color: #03045e;
+     padding: 20px;
+      font-size: 30px;
+       flex: 8;
+}
+footer {
+background-color: #03045e; 
+text-align: center; 
+font-size: 20px;
+flex: 1;
+}
+</style>
