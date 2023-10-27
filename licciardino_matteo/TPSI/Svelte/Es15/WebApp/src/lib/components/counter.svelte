8376e0baf6f6c47eb70a501be176858745236144
Licciardino-Matteo <matteo.licciardino@istitutoagnelli.it>
2023-10-27 12:23:22 +0200

    Fatto es 15 di Svelte

---
+++ licciardino_matteo/TPSI/Svelte/Es15/WebApp/src/lib/components/counter.svelte
@@ -0,0 +1,84 @@
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</svelte:head>
+
+<script>
+    import { createEventDispatcher } from "svelte";
+    import { store_counter } from '../js/store';
+
+    const dispatch = createEventDispatcher();
+    export let counter = 0;
+
+    function incre_counter(){
+        counter += 1;
+        $store_counter = counter;
+        emit_event("incr");
+    }
+
+    function decre_counter(){
+        counter -= 1;
+        $store_counter = counter;
+        emit_event("decr");
+    }
+
+    function emit_event(tipo){
+        dispatch("contatore", {
+            tipo: tipo,
+            valore: counter,
+        })
+    }
+</script>
+
+<div class="contanier">
+    <button class="pulsante" on:click={decre_counter}>
+        <span class="material-symbols-outlined icon">remove</span>
+    </button>
+    <div class="number">
+        {counter}
+    </div>
+    <button class="pulsante" on:click={incre_counter}>
+        <span class="material-symbols-outlined icon">add</span>
+    </button >
+</div>
+
+<style>
+    .contanier{
+        border: 4px solid #D43C2F;
+        display: flex;
+        justify-content: space-between;
+        border-radius: 30px;
+        width: 300px;
+        padding: 10px;
+    }
+
+    .pulsante{
+        display: flex;
+        align-items: center;
+    }
+
+    button{
+        border: none;
+        background: none;
+    }
+
+    .icon{
+        font-size: 2.5rem;
+        color: #F09C00;
+        font-weight: bolder;
+    }
+
+    .icon:hover{
+        color: #D43C2F;
+        cursor: pointer;
+    }
+
+    .icon:active{
+        text-shadow: 1px 1px 10px #D43C2F;
+    }
+
+    .number{
+        font-size: 4rem;
+        font-family: 'Bungee Spice', cursive;
+    }
+</style>
---
+++ licciardino_matteo/TPSI/Svelte/Es15/WebApp/src/lib/components/who_am_i.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let nome = "Matteo";
+    export let cognome = "Licciardino";
+    export let eta = 19;
+    let src = "https://static.vecteezy.com/ti/vettori-gratis/p1/2318271-icona-profilo-utente-vettoriale.jpg";
+    export let larghezza = 150;
+</script>
+
+<h1>Ciao io sono {nome} {cognome} ed ho {eta} anni</h1>
+<h4>Sono un studente di informatica presso l'istituto Agnelli a Torino</h4>
+
+<img {src} alt="Utente" width="{larghezza}">
