bc203b7aef80f8ef89c6f54b256e9d609951a430
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2023-10-27 12:18:09 +0200

    add es15

---
+++ scattaglia_alessandro/TPSI/Svelte/es15/src/routes/About/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { store_counter } from "../../lib/js/store";
+    import { store_counter1 } from "../../lib/js/store";
+</script>
+
+<h1>Questa è la pagina ABOUT</h1>
+
+<h2>Il contatore vale: {$store_counter}</h2>
+<h2>Il contatore1 vale: {$store_counter1}</h2>
