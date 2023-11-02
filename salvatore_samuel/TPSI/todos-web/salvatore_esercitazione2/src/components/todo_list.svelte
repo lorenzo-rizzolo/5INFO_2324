2788d10f39923ed5c60f81d6597a01488e41cb84
Samuel-Salvatore <samuel.salvatore@istitutoagnelli.it>
2023-11-02 16:44:22 +0100

    todos

---
+++ salvatore_samuel/TPSI/todos-web/salvatore_esercitazione2/src/components/todo_list.svelte
@@ -0,0 +1,37 @@
+<h1>ToDos</h1>
+<div class="todo-list">
+    <div class="header">H1</div>
+    <div class="header">H2</div>
+    <div class="header">H3</div>
+    <div class="header">H4</div>
+    <div class="header">H5</div>
+
+    <div>CELLA 1</div>
+    <div>CELLA 2</div>
+    <div>CELLA 3</div>
+    <div>CELLA 4</div>
+    <div>CELLA 5</div>
+
+    <div>CELLA 1</div>
+    <div>CELLA 2</div>
+    <div>CELLA 3</div>
+    <div>CELLA 4</div>
+    <div>CELLA 5</div>
+
+    <div>CELLA 1</div>
+    <div>CELLA 2</div>
+    <div>CELLA 5</div>
+    <div>CELLA 3</div>
+    <div>CELLA 4</div>
+
+</div>
+<style>
+    .todo-list{
+        display: grid;
+        grid-template-columns: 1fr 1fr 4fr 2fr 1fr;
+        border: 3px solid blue;
+        width: 95%;
+        height: 80%;
+        margin: auto;
+    }
+</style>
