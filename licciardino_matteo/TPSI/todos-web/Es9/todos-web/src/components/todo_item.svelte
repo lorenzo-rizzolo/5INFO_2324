724113421fe511d6e591ac3dcd553a90d01031bd
Licciardino-Matteo <matteo.licciardino@istitutoagnelli.it>
2023-11-02 16:28:03 +0100

    Fatto da 5 a 9 del ToDos WebApp

---
+++ licciardino_matteo/TPSI/todos-web/Es9/todos-web/src/components/todo_item.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Cell from "./cell.svelte";
+    import Icon from "./icon.svelte";
+    import Priority from "./priority.svelte";
+
+    export let todo;
+
+    const toggle_status = () => {
+        todo.done =! todo.done;
+    }
+</script>
+
+<Cell> {todo.id} </Cell>
+<Cell> 
+    {#if todo.done == false}
+        <Icon name="circle" handler={toggle_status}/>
+    {:else}
+        <Icon name="task_alt" handler={toggle_status}/>
+    {/if}
+</Cell>
+<Cell> {todo.task} </Cell>
+<Cell> <Priority/> </Cell>
+<Cell> <Icon name="delete_forever"/> </Cell>
