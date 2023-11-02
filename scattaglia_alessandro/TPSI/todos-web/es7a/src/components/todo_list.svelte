4b5ae6cf4d23258b59fedda8c36c811cbf4c205c
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2023-11-02 15:46:12 +0100

    add es7a

---
+++ scattaglia_alessandro/TPSI/todos-web/es7a/src/components/todo_list.svelte
@@ -0,0 +1,53 @@
+<script>
+    import Icon from "./icon.svelte";
+    import TodoItem from "./todo.item.svelte"
+    let todos = [
+        {
+            id:1,
+            task: "Studiare GPO",
+            done: false,
+            priority: 1
+        },
+
+        {
+            id:2,
+            task: "Studiare TPSI",
+            done: true,
+            priority: 2
+        },
+
+        {
+            id:3,
+            task: "Studiare RETI",
+            done: false,
+            priority: 3
+        },
+
+
+    ];
+</script>
+
+
+<h1>ToDos</h1>
+<div class="todo-list">
+    <div class="header"><Icon name="tag"/></div>
+    <div class="header"><Icon name="task_alt" /></div>
+    <div class="header"><Icon name="list" /></div>
+    <div class="header"><Icon name="schedule" /></div>
+    <div class="header"><Icon name="add_box"/></div>
+
+    <TodoItem todo={todos[0]}/>
+    <TodoItem todo={todos[1]}/>
+    <TodoItem todo={todos[2]}/>
+</div>
+
+<style>
+    .todo-list {
+        display: grid;
+        grid-template-columns: 1fr 1fr 4fr 2fr 1fr;
+        border: 3px solid blue;
+        width: 95%;
+        height: 80%;
+        margin:auto;
+    }
+</style>
