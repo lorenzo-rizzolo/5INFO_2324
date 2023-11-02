778be1e2d6fd58512cd409f57e9cbd7d6d232197
Luca-Porta <luca.porta@istitutoagnelli.it>
2023-11-02 16:43:41 +0100

    Aggiunta esercitazione 08

---
+++ porta_luca/TPSI/Svelte/Todos-Web/Esercitazione_08/src/components/Icon.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet"/>
+</svelte:head>
+
+<script>
+    export let name;
+    export let handler = () => {};
+</script>
+
+<main>
+    <span class="material-icons-outlined icon" on:click={handler}>
+    {name}
+    </span>
+</main>
+<style>
+    .icon {font-size: 32px;}
+    .icon:hover {cursor: pointer;}
+</style>
