7552c0d13472f33a3953b4b37b3595f191f6936b
Marco-Tranaso <marco.tranaso@istitutoagnelli.it>
2023-11-02 16:43:20 +0100

    add es5,es6,es7,es7a,es8,es9

---
+++ tranaso_marco/TPSI/todos-web/es7a/src/components/todo_list.svelte
@@ -0,0 +1,48 @@
+<script>
+    import Icon from "./icon.svelte";
+    import TodoItem from "./todo_item.svelte";
+    let todos=[{
+        id:1,
+        task:"Studiare TPSI",
+        done:false,
+        priority:1
+    },
+    {
+        id:1,
+        task:"Studiare Reti",
+        done:false,
+        priority:1
+    },
+    {
+        id:3,
+        task:"Studiare Informatica",
+        done:false,
+        priority:3
+    }
+    ]
+</script>
+
+<h1>ToDos</h1>
+<div class="todo-list">
+    <div class="header"><Icon name="tag" /></div>
+    <div class="header"><Icon name="task_alt" /></div>
+    <div class="header"><Icon name="list" /></div>
+    <div class="header"><Icon name="schedule" /></div>
+    <div class="header"><Icon name="add_box" /></div>
+
+    <TodoItem todo={todos[0]}/>
+    <TodoItem todo={todos[1]}/>
+    <TodoItem todo={todos[2]}/>
+
+</div>
+
+<style>
+    .todo-list {
+        display: grid;
+        grid-template-columns: 1fr 1fr 4fr 2fr 1fr;
+        border: 3px solid blue;
+        width: 95%;
+        height: 80%;
+        margin: auto;
+    }
+</style>
