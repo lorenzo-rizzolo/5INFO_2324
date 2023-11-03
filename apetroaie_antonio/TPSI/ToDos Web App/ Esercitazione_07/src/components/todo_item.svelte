499a1196536df042e987bfe5e94c9bf802417eb3
profmancusoa <profmancusoa@gmail.com>
2023-11-03 13:42:14 +0100

    cleanup

--- apetroaie_antonio/TPSI/ToDos Web App/ Esercitazione_07/src/components/todo_item.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Cell from "./cell.svelte";
-</script>
-
-<Cell/>
-<Cell/>
-<Cell/>
-<Cell/>
-<Cell/>
--- apetroaie_antonio/TPSI/ToDos Web App/ Esercitazione_07a/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import TodoList from "../components/todo_list.svelte";
-
-
-</script>
-
-<TodoList></TodoList>
