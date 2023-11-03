a633df3e741be6cf7f8d392b76fa84e632ea0709
Marco-Tranaso <marco.tranaso@istitutoagnelli.it>
2023-11-03 13:36:37 +0100

    es 10,11,12,13,14

---
+++ tranaso_marco/TPSI/todos-web/es10/src/components/icon.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let name;
+    export let handler=()=>{};
+</script>
+
+
+<svelte:head>
+    <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
+        rel="stylesheet">
+
+</svelte:head>
+<span class="material-icons-outlined icon" on:click={handler}>
+    {name}
+</span>
+
+<style>
+    div{
+        border: 2px solid green;
+        width: 64px;
+        height: 64px;
+        margin:5px;
+    }
+    .icon{
+        font-size: 32px;
+    }
+    .icon:hover{
+        cursor: pointer;
+    }
+</style>
