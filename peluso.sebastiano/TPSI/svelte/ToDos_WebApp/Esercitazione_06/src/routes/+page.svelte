f7b321503510b3ca15c1624a8fecc1ed503bba27
Sebastiano-Peluso <sebastiano.peluso@istitutoagnelli.it>
2023-11-07 00:41:13 +0100

    add Esercitazione_06 e 07

---
+++ peluso.sebastiano/TPSI/svelte/ToDos_WebApp/Esercitazione_06/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Todolist from '../lib/components/todo_list.svelte';
+</script>
+
+
+<main>
+    <h1>To dos</h1>
+    
+
+    <Todolist/>
+</main>
+
+
+
+
+<style>
+    main{
+        border: 3px solid red;
+        width: 60%;
+        height: 90vh;
+        margin: auto;
+    }
+</style>
