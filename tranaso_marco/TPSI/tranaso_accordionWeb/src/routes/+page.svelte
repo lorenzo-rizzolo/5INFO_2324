e959d0a9ade742b7a7b4ed08d80c15b637772e71
Marco-Tranaso <marco.tranaso@istitutoagnelli.it>
2023-11-09 22:27:25 +0100

    accordion

---
+++ tranaso_marco/TPSI/tranaso_accordionWeb/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Accordion_1 from "../lib/components/accordion_1.svelte";
+    import Accordion_2 from "../lib/components/accordion_2.svelte";
+</script>
+
+<div class="container">
+    <div class="item">
+        <Accordion_1/>
+    </div>
+    <div class="item">
+        <Accordion_2/>
+    </div>
+
+</div>
