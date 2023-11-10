8af0b7e5d1adcf0396f6f7a6a93380ad39026d87
Licciardino-Matteo <matteo.licciardino@istitutoagnelli.it>
2023-11-10 17:15:45 +0100

    Fatto TPSI da 15 a 21

---
+++ licciardino_matteo/TPSI/todos-web/Es21/todos-web/src/components/cell.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { fade } from "svelte/transition";
+
+    export let last = false;
+</script>
+
+<div class="cell {last ? 'cell-last' : ''}" in:fade="{{duration: 700}}" on:fade="{{duration: 300}}">
+    <slot/>
+</div>
+
+<style>
+    .cell{
+        border-bottom: 1px solid #E7ECEE;
+        border-right: 1px solid #E7ECEE;
+        text-align: center;
+        padding-top: 10px;
+        color: #525252;
+        line-height: 32px;
+        font-size: 18px;
+    }
+
+    .cell-last{
+        border-right: none;
+    }
+</style>
