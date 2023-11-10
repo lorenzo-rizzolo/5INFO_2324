dbb59fa7c5a8d8f40361261508d34f5d1c099ad5
Minta-Darkwah <ohenebaminta.darkwah@istitutoagnelli.it>
2023-11-10 02:01:48 +0100

    aggiunta es.2

---
+++ darkwah_minta/TPSI/Sveltekit/darkwah_accordionWeb/src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    import Titolo1 from "../lib/components/titolo_sopra.svelte";
+    import Titolo2 from "../lib/components/titolo_sotto.svelte";
+    import Paragrafo1 from "../lib/components/paragrafo1.svelte";
+    import Paragrafo2 from "../lib/components/paragrafo2.svelte";
+
+    let num = 2;
+    
+    const change_prio = () => {
+        num = (num + 1) % 2 + 1;
+        
+    }
+</script>
+
+
+<br/><br/><br/>
+<div id="tutto">
+    
+    <Titolo1 />
+    {#if num == 1}
+        <Paragrafo1/>
+    {/if}
+    <Titolo2 on:click={change_prio}/>
+    {#if num == 2}
+        <Paragrafo2/>
+    {/if}
+    
+    
+</div>
+
+<style>
+   
+    #tutto
+    {
+        width: 45rem;
+        height: 27rem;
+
+        display: flex;
+        flex-direction: column;
+
+        
+        border-radius: 1rem 1rem 0rem 0rem;
+        margin: auto;
+
+    }
+</style>
