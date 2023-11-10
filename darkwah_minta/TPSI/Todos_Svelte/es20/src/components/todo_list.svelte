2df2906804edefbed04b6a12d7cadbd059cb19f2
Minta-Darkwah <ohenebaminta.darkwah@istitutoagnelli.it>
2023-11-10 13:50:46 +0100

    aggiunta es.20

---
+++ darkwah_minta/TPSI/Todos_Svelte/es20/src/components/todo_list.svelte
@@ -0,0 +1,115 @@
+<script>
+    import Icon from "./icon.svelte";
+    import TodoItem from "./todo_item.svelte";
+
+    import {onMount} from "svelte";
+
+    let todos = [];
+    let last_id = 0;
+
+    const create_todo = async () => {
+        let todo = {
+            id: ++last_id,
+            task: '',
+            done: false,
+            priority: 3
+        };
+
+        console.log("CREATE:", todo);
+
+        //inserisco il nuovo todo nel localStorage
+        localStorage.setItem(`todo${todo.id}`, JSON.stringify(todo));
+        
+        //aggiorno la nodo list
+        todos = [...todos, todo];
+
+    }
+
+    const change_todo_item = async(e) => {
+        switch(e.detail.type) 
+        {
+            case 'update':
+                update_item(e.detail.id);
+                break;
+            case 'delete':
+                delete_item(e.detail.id);
+                break;
+        }
+    }
+
+    const update_item = (id) => {
+        console.log("UPDATE:", id);
+
+        //aggiorno il localstorage
+        const todo = todos.filter(t => t.id == id)[0];
+        localStorage.setItem(`todo${id}`, JSON.stringify(todo));
+    }
+
+    const delete_item = (id) => {
+        console.log("DELETE:", id);
+        todos = todos.filter(t => t.id != id);
+
+        //rimuovo il todo dal localStorage
+        localStorage.removeItem(`todo${id}`);
+    }
+
+    onMount(async () => {
+        for (let i = 0; i < localStorage.length; i++)
+        {
+            const key = localStorage.key(i);
+            const todo = JSON.parse(localStorage.getItem(key));
+
+            if (todo != null)
+                todos.push(todo);
+            
+        }
+        todos = [...todos];
+    });
+</script>
+
+<h1 class="app-title">ToDos</h1>
+
+<div class="todo-list">
+    <div class="header"><Icon name="tag"/></div>
+    <div class="header"><Icon name="task_alt"/></div>
+    <div class="header"><Icon name="list"/></div>
+    <div class="header"><Icon name="schedule"/></div>
+    <div class="header header-last"><Icon name="add_box" handler={create_todo}/></div>
+    
+    {#each todos as todo}
+        <TodoItem todo={todo} on:change={change_todo_item}/>
+    {/each}
+
+
+   </div>
+
+<style>
+
+    @import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap');
+    
+    .app-title {
+        font-family: 'Permanent Marker', cursive;
+        margin-top: 15px;
+        font-size: 60px;
+        opacity: 0.3;
+    } 
+    
+    .todo-list {
+        display: grid;
+        grid-template-columns: 1fr 1fr 4fr 2fr 1fr;
+        border: 0px solid blue;
+        width: 95%;
+        margin: auto;
+    }
+
+    .header {
+        border-bottom: 1px solid #E7ECEE;
+        border-right: 1px solid #E7ECEE;
+        text-align: center;
+        padding-bottom: 20px;
+    }
+
+    .header-last {
+        border-right: none;
+    }
+</style>
---
+++ darkwah_minta/TPSI/Todos_Svelte/es20/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import TodoList from "../components/todo_list.svelte";
+</script>
+
+<main>
+    <TodoList/>
+</main>
+
+<style>
+    main
+    {
+        border: 0px solid red;
+        width: 60%;
+        height: 90vh;
+        margin: auto;
+    }
+</style>
