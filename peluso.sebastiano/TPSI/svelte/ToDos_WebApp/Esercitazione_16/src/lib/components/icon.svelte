615804904ec14c030de145a3fdc61244a81ef33e
SebastianoPeluso <sebastiano.peluso@istitutoagnelli.it>
2023-11-11 22:51:32 +0100

    add Esercitazioni da 13 a 21

---
+++ peluso.sebastiano/TPSI/svelte/ToDos_WebApp/Esercitazione_16/src/lib/components/icon.svelte
@@ -0,0 +1,33 @@
+
+
+<svelte:head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined">
+</svelte:head>
+
+<script>
+    export let name;
+    export let color = "green";
+
+    export let handler = () => {};
+</script>
+
+
+<span class="material-icons-outlined icon" on:click={handler} style="--color:{color};" >
+    {name}
+</span>
+
+<style>
+
+
+    .icon{
+        color: var(--color);
+        
+        font-size: 32px;
+        
+    }
+
+    .icon:hover{
+        cursor: pointer;
+    }
+
+</style>
---
+++ peluso.sebastiano/TPSI/svelte/ToDos_WebApp/Esercitazione_21/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Todolist from '../lib/components/todo_list.svelte';
+</script>
+
+
+<main>
+    
+
+    <Todolist/>
+</main>
+
+
+
+
+<style>
+    main{
+        border: 0px solid red;
+        width: 60%;
+        margin: auto;
+    }
+</style>
