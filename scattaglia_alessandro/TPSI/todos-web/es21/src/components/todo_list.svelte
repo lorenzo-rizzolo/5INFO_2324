e32c0ee9d56e855aa47b10887bf8ecfc2050994a
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2023-11-13 13:12:43 +0100

    add es21

---
+++ scattaglia_alessandro/TPSI/todos-web/es21/src/components/todo_list.svelte
@@ -0,0 +1,97 @@
+<script>
+    import Icon from "./icon.svelte";
+    import TodoItem from "./todo.item.svelte";
+    import { onMount } from "svelte";
+    let last_id = 0;
+    let todos = [];
+    const create_todo = async () => {
+        let todo = {
+            id: ++last_id,
+            task: "",
+            done: false,
+            priority: 3,
+        };
+        console.log("CREATE:", todo);
+        localStorage.setItem(`todo${todo.id}`, JSON.stringify(todo));
+
+        todos = [...todos, todo];
+    };
+
+    const change_todo_item = async (e) => {
+        switch (e.detail.type) {
+            case "update":
+                update_item(e.detail.id);
+                break;
+            case "delete":
+                delete_item(e.detail.id);
+                break;
+        }
+    };
+    const update_item = (id) => {
+        console.log("UPDATE", id);
+        const todo = todos.filter((t) => t.id == id)[0];
+        localStorage.setItem(`todo${id}`, JSON.stringify(todo));
+    };
+    const delete_item = (id) => {
+        console.log("DELETE:", id);
+        todos = todos.filter((t) => t.id != id);
+        localStorage.removeItem(`todo${id}`);
+    };
+
+    onMount(async () => {
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const keyn = +key.substring(4);
+            if(keyn >= last_id)
+            {
+                last_id = keyn;
+            }
+            const todo = JSON.parse(localStorage.getItem(key));
+            if (todo != null) {
+                todos.push(todo);
+            }
+        }
+    });
+</script>
+
+<h1 class="app-title">ToDos</h1>
+<div class="todo-list">
+    <div class="header"><Icon name="tag" /></div>
+    <div class="header"><Icon name="task_alt" /></div>
+    <div class="header"><Icon name="list" /></div>
+    <div class="header"><Icon name="schedule" /></div>
+    <div class="header header-last">
+        <Icon name="add_box" handler={create_todo} />
+    </div>
+
+    {#each todos as todo}
+        <TodoItem {todo} on:change={change_todo_item} />
+    {/each}
+</div>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap");
+    .todo-list {
+        display: grid;
+        grid-template-columns: 1fr 1fr 4fr 2fr 1fr;
+        border: 3px solid blue;
+        width: 95%;
+        height: 80%;
+        margin: auto;
+    }
+    .app-title {
+        font-family: "Permanent Marker", cursive;
+        margin-top: 0px;
+        font-size: 60px;
+        opacity: 0.3;
+    }
+    .header {
+        border-bottom: 1px solid #e7ecee;
+        border-right: 1px solid #e7ecee;
+        text-align: center;
+        padding-bottom: 20px;
+    }
+    .header-last {
+        border-right: none;
+    }
+</style>
