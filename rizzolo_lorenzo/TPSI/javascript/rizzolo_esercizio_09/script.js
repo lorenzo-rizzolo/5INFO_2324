ee2ea05e3790b88b48fdb3ec04a9fc8a2426a4f1
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2023-11-24 12:45:58 +0100

    rizzolo esercizi js

---
+++ rizzolo_lorenzo/TPSI/javascript/rizzolo_esercizio_09/script.js
@@ -0,0 +1,15 @@
+
+// let admin = prompt("Insert your username")=="admin" || alert("I don't know you")
+
+// console.log(admin)
+
+// let password = admin && ( !( prompt("Password ?")=="Agnell1no" && alert("Welcome") ) || alert("Wrong password") )
+
+let user = prompt("Username")
+
+user || alert("Canceled")
+user=="admin" || alert("I don't know you")
+user=="admin" && (password = prompt("Password"))
+password || alert("Canceled")
+password=="Agnell1no" || alert("Wrong password")
+password=="Agnell1no" && alert("Welcome")
